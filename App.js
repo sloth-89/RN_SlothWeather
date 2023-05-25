79f117c3c132785f42e088dda7d58f1c5b8e945a
restvoice2020@gmail.com <restvoice2020@gmail.com>
2023-05-25 10:40:18 +0900

    the rules of naive

--- App.js
+++ App.js
@@ -3,18 +3,26 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    // <div/> 대신 <View/>, 글자는 <Text/> 컴포넌트 안에
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello!</Text>
+      {/* 휴대폰 상단 상태바 */}
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// 스타일을 클래스를 정의하는 것처럼 따로 정의하려 사용 할 수 있다.
+// css 98% 정도의 속성들 사용 가능
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 28,
+    color: "red",
   },
 });
